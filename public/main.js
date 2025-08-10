b3191577e1707db26a29ccf47f7872a0c84f1ffc
mariaruban80 <maria.ruban80@gmail.com>
2025-08-10 14:58:21 +0100

    Update main.js

--- public/main.js
+++ public/main.js
@@ -218,7 +218,7 @@ if (uploadTicketBtn) {
         menu.classList.remove('show');
     });
 }
-}
+
 
 
   /** ---------- IMPORT BUTTON IN MODAL ---------- **/
@@ -4403,6 +4403,7 @@ window.addEventListener('beforeunload', () => {
     clearInterval(heartbeatInterval);
   }
 });
+
 
 
 
